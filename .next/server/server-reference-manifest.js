18dabf01644f98d0bd67c3d8ae22557a7b12f814
AlexRDev <alex.ramirez@beu.app>
2025-06-24 13:56:46 -0500

    fix: correccion, se cambian los imports por falta de consistencia

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cmdwMxcuST//JkahCRiQmtfcAYbFmB07MoJ4HIOn4QI=\"\n}"
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,8 @@
-import Home from '../components/Home';
+import Home from '../components/Home.jsx';
+import Header from '../components/Header.jsx'
+import About from '../components/About.jsx'
+import Portfolio from '../components/Portfolio.jsx'
+import Contact from '../components/Contact.jsx'
 
 export default function HomePage() {
   return <Home />;
